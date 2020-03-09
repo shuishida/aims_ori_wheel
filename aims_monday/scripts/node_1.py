a9b9cc28d95a4df175bf2133aa2b0999f382ca8f
Jonas Beuchert <jonas.beuchert@eng.ox.ac.uk>
2020-03-09 14:56:04 +0000

    Not working yet.

--- aims_monday/scripts/node_1.py
+++ aims_monday/scripts/node_1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+# from std_msgs.msg import String
 from geometry_msgs.msg import Twist, Vector3
 from math import pi
 
@@ -13,22 +13,17 @@ if __name__ == "__main__":
 	#	pub.publish('Hello AIMS.')
 	#	r.sleep()
 	msg = Twist()
-	msgTurn = Twist()
+	msg_turn = Twist()
 	msg.linear.x = 0.1
-	msgTurn.angular.z = pi / 10.0
+	msg_turn.angular.z = pi / 10.0
 	pub = rospy.Publisher('cmd_vel', Twist)
-	i = 0
-	while i < 30: # not rospy.is_shutdown():
+	for i in range(30): # not rospy.is_shutdown():
 		pub.publish(msg)
 		r.sleep()
-		i = i + 1
-	i = 0
-	while i < 100:
-		pub.publish(msgTurn)
+	for i in range (100):
+		pub.publish(msg_turn)
 		r.sleep()
-		i = i + 1
-	i = 0
-	while i < 30:
+	for i in range(30):
 		pub.publish(msg)
 		r.sleep()
-		i = i + 1
+	#r.spin()
