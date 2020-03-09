a3b0384cc419527a576eab7f5661dcb5651e4bbb
Jonas Beuchert <jonas.beuchert@eng.ox.ac.uk>
2020-03-09 15:14:21 +0000

    Now working. node_3, request, and node_2 together.

--- aims_monday/scripts/node_3.py
+++ aims_monday/scripts/node_3.py
@@ -4,23 +4,32 @@ import rospy
 from std_msgs.msg import String
 from std_srvs.srv import SetBool, SetBoolResponse
 
-flag = False
+class main_class:
+	
+	def __init__(self):
+		self.flag = False
+		self.s = rospy.Service('receive_bool', SetBool, self.handle_bool)
 
-def handle_bool(req):
-	# Request is of type SetBoolRequest
-	print('Received %s' % req.data)
-	flag = req.data
-	return SetBoolResponse(success=True, message='Received %s' % req.data)
+	def handle_bool(self, req):
+		# Request is of type SetBoolRequest
+		print('Received %s' % req.data)
+		rospy.loginfo("logging")
+		self.flag = req.data
+		return SetBoolResponse(success=True, message='Received %s' % req.data)
 
-if __name__ == "__main__":
-	s = rospy.Service('receive_bool', SetBool, handle_bool)
-	#rospy.spin()
-	pub = rospy.Publisher('phrases', String)
-	rospy.init_node('speaker')
-	r = rospy.Rate(10)
-	while not rospy.is_shutdown():
-		if flag:
-			pub.publish('Hello AIMS.')
+	def running(self):
+		#rospy.spin()
+		pub = rospy.Publisher('phrases', String)
+		rospy.init_node('speaker')
+		r = rospy.Rate(10)
+		while not rospy.is_shutdown():
+			print(self.flag)
+			if self.flag:
+				pub.publish('Hello AIMS.')
 			r.sleep()
 
-	
+
+if __name__ == "__main__":
+	m = main_class()
+	m.running()
+		
