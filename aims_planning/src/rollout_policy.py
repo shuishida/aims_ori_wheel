6debeb425ff0e6f638c2b08f1ec027089fb9f02e
Jonas Beuchert <57848364+JonasBchrt@users.noreply.github.com>
2020-03-12 08:25:39 +0000

    Advanced rollout policy during exploration.

--- aims_planning/src/rollout_policy.py
+++ aims_planning/src/rollout_policy.py
@@ -39,6 +39,14 @@ def rollout_policy(state, action):
         # If can check for rubble
         
             # Check for rubble
+            
+        # If in front of unsearched room
+        
+            # Go into room
+            
+        # If can move in front of unsearched room
+        
+            # Move in front of unsearched room
 
     if 'finish' in action:
         return 'finish'
@@ -60,4 +68,20 @@ def rollout_policy(state, action):
             return 'clear_rubble'
         if 'check_for_rubble' in action:
             return 'check_for_rubble'
+    if state['room_1_person'] == 'unknown' and 'WayPoint8' in action: # Room 1 unsearched and in front of room 1
+        return 'WayPoint8'
+    if state['room_2_person'] == 'unknown' and 'WayPoint7' in action: # Room 2 unsearched and in front of room 2
+        return 'WayPoint7'
+    if state['room_3_person'] == 'unknown' and 'WayPoint5' in action: # Room 3 unsearched and in front of room 3
+        return 'WayPoint5'
+    if state['room_4_person'] == 'unknown' and 'WayPoint6' in action: # Room 4 unsearched and in front of room 4
+        return 'WayPoint6'
+    if state['room_1_person'] == 'unknown' and 'WayPoint4' in action: # Room 1 unsearched and can move in front of room 1
+        return 'WayPoint4'
+    if state['room_2_person'] == 'unknown' and 'WayPoint2' in action: # Room 2 unsearched and can move in front of room 2
+        return 'WayPoint2'
+    if state['room_3_person'] == 'unknown' and 'WayPoint1' in action: # Room 3 unsearched and can move in front of room 3
+        return 'WayPoint1'
+    if state['room_4_person'] == 'unknown' and 'WayPoint3' in action: # Room 4 unsearched and can move in front of room 4
+        return 'WayPoint3'
    return np.random.choice(action)
